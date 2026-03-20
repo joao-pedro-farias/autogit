687b09a0b09ade751066275e9d83fcf671a4cab6
joao-pedro-farias <joaopedro.esf@gmail.com>
2026-03-20 02:05:11 -0300

    primeiro commit

--- verpasta.py
+++ verpasta.py
@@ -9,19 +9,31 @@ else:
     print("subindo pasta para o git")
     subprocess.run(['git', 'init'], check=True)
 
-resposta = input("Deseja subir os arquivos para o git? (s/n)")
+resposta = input("Deseja subir os arquivos para o git? (s/n): ")
 
 if resposta.lower() == 's':
     print("Subindo")
     subprocess.run(['git', 'add', '.'])
-    commit = input("Deseja escrever seu commit? (Enter = auto commit)")
+    commit = input("Deseja escrever seu commit? (Enter = auto commit): ")
     if commit.strip() == "":
         print("Commit automatico")
         subprocess.run(['git', 'commit', '-m', 'auto commit'])
     else:
         print("Sua mensagem de commit:", commit)
         subprocess.run(['git', 'commit', '-m', commit])
-
+    url = input("Cole a URL do repositorio aqui: ")
+    subprocess.run(['git', 'remote', 'add', 'origin', url])
+    branch = input("Deseja subir para outra branch? (Enter = main): ")
+    if branch.strip() == "":
+        print("Subindo para branch main")
+        subprocess.run(['git', 'branch', '-M', 'main'])
+        print("dando Push para main")
+        subprocess.run(['git', 'push', '-u', 'origin', 'main'])
+    else:
+        print("subindo para branch ", branch)
+        subprocess.run(['git', 'branch', '-M', branch])
+        print("dando push para ", branch)
+        subprocess.run(['git', 'push', '-u', 'origin', branch])
 
 
 
